d43f314e363f279a36f291269919fa7feb795439
MohammedJabivullah <mohammadjabivullah07@gmail.com>
2026-08-25 21:26:47 +0530

    first commit

---
+++ src/FizzBuzz.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public List<String> fizzBuzzMethod(int n){
+        List<String> answers = new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%15==0){
+                answers.add("FizzBuzz");
+            }else if(i%3==0){
+                answers.add("Fizz");
+            }else if(i%5==0){
+                answers.add("Buzz");
+            }else{
+                answers.add(String.valueOf(i));
+            }
+        }
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number:");
+        int num = sc.nextInt();
+
+         FizzBuzz fizzBuzz = new FizzBuzz();
+        List<String> fizzBuzzList = fizzBuzz.fizzBuzzMethod(num);
+        System.out.print("\n[");
+        for(String s : fizzBuzzList){
+
+            System.out.print( " \""+s+"\"");
+        }
+        System.out.print(" ]");
+
+    }
+}
