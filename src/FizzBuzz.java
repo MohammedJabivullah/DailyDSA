844f493d34395ab181eb953677540741c06c3cd9
MohammedJabivullah <mohammadjabivullah07@gmail.com>
2026-08-25 21:41:31 +0530

    412 Fizz Buzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,6 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//Problem 412. Fizz Buzz
+
+//Given an integer n, return a string array answer (1-indexed) where:
+//
+//answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+//answer[i] == "Fizz" if i is divisible by 3.
+//answer[i] == "Buzz" if i is divisible by 5.
+//answer[i] == i (as a string) if none of the above conditions are true.
+//https://leetcode.com/problems/fizz-buzz/description/
+
 public class FizzBuzz {
     public List<String> fizzBuzzMethod(int n){
         List<String> answers = new ArrayList<>();
